cb67901678f032a1b181470643c04a4b6669f475
notsreeraj <109159574+notsreeraj@users.noreply.github.com>
2025-02-11 12:58:53 -0500

    test 3

--- EuchreOOP3/Program.cs
+++ EuchreOOP3/Program.cs
@@ -16,7 +16,7 @@ namespace EuchreOOP3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmSplash());
         }
     }
 }
